66da4e0914a5843f81d63392872c6f0db163d005
DVArt <dvart@hotmail.com>
2022-10-07 18:09:49 +0300

    Add project files.

---
+++ L1_OOP/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1_OOP
+{
+    internal class Program
+    {
+        public string field; //Поле
+
+        public void Method()
+        {
+            Console.WriteLine(field); //Метод
+        }
+
+
+
+    }
+
+
+}
